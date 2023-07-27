0efe43705e7dbf91aefa05f0dc640f83d93abc44
Sergey Marunich <marunich.s@gmail.com>
2023-07-27 12:57:37 -0400

    Update demo.sh

--- make/demo.sh
+++ make/demo.sh
@@ -22,6 +22,17 @@ init_kubeconfig() {
   )
 }
 
+
+init_kubeconfig() {
+  local index="$1"
+  local cluster_name="$2"
+  local region="$3"
+  command="eksctl utils write-kubeconfig --cluster ${cluster_name} --region ${region} --kubeconfig .kube/config"
+  (    
+    run_command_at_jumpbox $index $command
+  )
+}
+
 if [[ ${ACTION} = "deploy_bookinfo" ]]; then
 	export index=0
   export AWS_K8S_CLUSTERS=$(echo ${TFVARS} | jq -c ".k8s_clusters.aws")
