857a5e09c1cc65efc10e8bac7b047c81cc405b9d
Sergey Marunich <marunich.s@gmail.com>
2023-08-22 15:33:34 -0400

    Re-structure demo.sh

--- make/demo.sh
+++ make/demo.sh
@@ -22,9 +22,12 @@ init_kubeconfig() {
   )
 }
 
-if [[ ${ACTION} = "deploy-application" ]] || [[ ${ACTION} = "01-deploy-application" ]] || [[ ${ACTION} = "01" ]] ; then
-  print_info "Deploying application... as per https://docs.tetrate.io/service-express/getting-started/deploy-application"
-	export index=0
+deploy_action() {
+  local action_name="${1}"
+  local url_suffix="${2}"
+  local script_name="${3}"
+  print_info "${action_name}... as per https://docs.tetrate.io/service-express/getting-started/${url_suffix}"
+  export index=0
   export AWS_K8S_CLUSTERS=$(echo ${TFVARS} | jq -c ".k8s_clusters.aws")
   cluster_name=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].name')
   region=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].region')
@@ -33,71 +36,25 @@ if [[ ${ACTION} = "deploy-application" ]] || [[ ${ACTION} = "01-deploy-applicati
     cluster_name=$NAME_PREFIX-$index-$region
   fi
   init_kubeconfig $index $cluster_name $region
-  print_info "Check if demo directory exists, if not create..."
-  run_command_at_jumpbox $index "if [ ! -d demo ]; then git clone ${GIT_REPO}; cp -r tetrate-service-express-sandbox/demo . ;fi"
-  run_command_at_jumpbox $index './demo/scripts/getting_started_guide/01-deploy-application.sh'
-fi
+  run_command_at_jumpbox "$index" "if [ ! -d demo ]; then git clone ${GIT_REPO}; cp -r tetrate-service-express-sandbox/demo . ;fi"
+  run_command_at_jumpbox "$index" "./demo/scripts/getting_started_guide/${script_name}"
+}
 
-if [[ ${ACTION} = "mtls" ]] || [[ ${ACTION} = "02-mtls" ]] || [[ ${ACTION} = "02" ]] ; then
-  print_info "Enforce Encryption with Mutual TLS... as per https://docs.tetrate.io/service-express/getting-started/mtls"
-	export index=0
-  export AWS_K8S_CLUSTERS=$(echo ${TFVARS} | jq -c ".k8s_clusters.aws")
-  cluster_name=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].name')
-  region=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].region')
-  k8s_version=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].version')
-  if [[ "$cluster_name" == "null" ]]; then
-    cluster_name=$NAME_PREFIX-$index-$region
-  fi
-  init_kubeconfig $index $cluster_name $region
-  print_info "Check if demo directory exists, if not create..."
-  run_command_at_jumpbox $index "if [ ! -d demo ]; then git clone ${GIT_REPO}; cp -r tetrate-service-express-sandbox/demo . ;fi"
-  run_command_at_jumpbox $index './demo/scripts/getting_started_guide/02-mtls.sh'
-fi
-
-if [[ ${ACTION} = "zero-trust" ]] || [[ ${ACTION} = "03-zero-trust" ]] || [[ ${ACTION} = "03" ]] ; then
-  print_info "Enforce A Zero-Trust Security Policy... as per https://docs.tetrate.io/service-express/getting-started/zero-trust"
-	export index=0
-  export AWS_K8S_CLUSTERS=$(echo ${TFVARS} | jq -c ".k8s_clusters.aws")
-  cluster_name=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].name')
-  region=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].region')
-  k8s_version=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].version')
-  if [[ "$cluster_name" == "null" ]]; then
-    cluster_name=$NAME_PREFIX-$index-$region
-  fi
-  init_kubeconfig $index $cluster_name $region
-  print_info "Check if demo directory exists, if not create..."
-  run_command_at_jumpbox $index "if [ ! -d demo ]; then git clone ${GIT_REPO}; cp -r tetrate-service-express-sandbox/demo . ;fi"
-  run_command_at_jumpbox $index './demo/scripts/getting_started_guide/03-zero-trust.sh'
-fi
-
-if [[ ${ACTION} = "publish-service" ]] || [[ ${ACTION} = "04-publish-service" ]] || [[ ${ACTION} = "04" ]] ; then
-  print_info "Publishing a Service... as per https://docs.tetrate.io/service-express/getting-started/publish-service"
-	export index=0
-  export AWS_K8S_CLUSTERS=$(echo ${TFVARS} | jq -c ".k8s_clusters.aws")
-  cluster_name=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].name')
-  region=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].region')
-  k8s_version=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].version')
-  if [[ "$cluster_name" == "null" ]]; then
-    cluster_name=$NAME_PREFIX-$index-$region
-  fi
-  init_kubeconfig $index $cluster_name $region
-  print_info "Check if demo directory exists, if not create..."
-  run_command_at_jumpbox $index "if [ ! -d demo ]; then git clone ${GIT_REPO}; cp -r tetrate-service-express-sandbox/demo . ;fi"
-  run_command_at_jumpbox $index './demo/scripts/getting_started_guide/04-publish-service.sh'
-fi
-
-if [[ ${ACTION} = "publish-api" ]] || [[ ${ACTION} = "05-publish-api" ]] || [[ ${ACTION} = "05" ]] ; then
-  print_info "Publishing an API from the OAS definition... as per https://docs.tetrate.io/service-express/getting-started/publish-api"
-	export index=0
-  export AWS_K8S_CLUSTERS=$(echo ${TFVARS} | jq -c ".k8s_clusters.aws")
-  cluster_name=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].name')
-  region=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].region')
-  k8s_version=$(echo $AWS_K8S_CLUSTERS | jq -cr '.['$index'].version')
-  if [[ "$cluster_name" == "null" ]]; then
-    cluster_name=$NAME_PREFIX-$index-$region
-  fi
-  init_kubeconfig $index $cluster_name $region
-  print_info "Check if demo directory exists, if not create..."
-  run_command_at_jumpbox $index "if [ ! -d demo ]; then git clone ${GIT_REPO}; cp -r tetrate-service-express-sandbox/demo . ;fi"
-  run_command_at_jumpbox $index './demo/scripts/getting_started_guide/05-publish-api.sh'
-fi
+case "${ACTION}" in
+  describe) source "${ROOT_DIR}/describe.sh" demo ;;
+  deploy-application | 01-deploy-application | 01)
+    deploy_action "Deploying application" "deploy-application" "01-deploy-application.sh"
+    ;;
+  mtls | 02-mtls | 02)
+    deploy_action "Enforce Encryption with Mutual TLS" "mtls" "02-mtls.sh"
+    ;;
+  zero-trust | 03-zero-trust | 03)
+    deploy_action "Enforce A Zero-Trust Security Policy" "03-zero-trust.sh"
+    ;;
+  publish-service | 04-publish-service | 04)
+    deploy_action "Publishing a Service" "publish-service" "04-publish-service.sh"
+    ;;
+  publish-api | 05-publish-api | 05)
+    deploy_action "Publishing an API from the OAS definition" "publish-api" "05-publish-api.sh"
+    ;;
+esac
